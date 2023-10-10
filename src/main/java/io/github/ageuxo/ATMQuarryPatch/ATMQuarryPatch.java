c147f5ffecf8fa11ca46b8886aed67b9bd0d0c55
ageuxo <ageuxo@gmail.com>
2023-10-10 16:27:44 +0200

    Fix naively creating a player for every block break Remove unneeded event used for debugging

--- src/main/java/io/github/ageuxo/ATMQuarryPatch/ATMQuarryPatch.java
+++ src/main/java/io/github/ageuxo/ATMQuarryPatch/ATMQuarryPatch.java
@@ -1,19 +1,26 @@
 package io.github.ageuxo.ATMQuarryPatch;
 
+import com.mojang.authlib.GameProfile;
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.data.event.GatherDataEvent;
-import net.minecraftforge.event.level.BlockEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Mod("atm_quarry_patch")
 public class ATMQuarryPatch {
@@ -21,19 +28,26 @@ public class ATMQuarryPatch {
     private static final Logger LOGGER = LogUtils.getLogger();
     public static final TagKey<Block> BREAKABLE_TAGKEY = TagKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation(ATMQuarryPatch.MODID, "breakable"));
 
+    public static final UUID FAKE_PLAYER_ID = UUID.randomUUID();
+    public static final GameProfile FAKE_PROFILE = new GameProfile(FAKE_PLAYER_ID, "ATMQuarryPatch FakePlayer");
 
-    @Mod.EventBusSubscriber
-    public static class Events{
-        @SubscribeEvent(receiveCanceled = true, priority = EventPriority.LOWEST)
-        public static void onBlockBreak(BlockEvent.BreakEvent event){
-            BlockState state = event.getState();
-            LOGGER.debug("{} {} {}", event.isCanceled(), event.getPlayer(), state);
-            if (state.is(BREAKABLE_TAGKEY)) {
-                event.setCanceled(false);
-                LOGGER.debug("{}", event.isCanceled());
+    private static final Map<ServerLevel, FakePlayer> PLAYER_MAP = new HashMap<>();
+
+    public static Player getFakePlayer(Level level){
+        if (level instanceof ServerLevel serverLevel){
+            FakePlayer fakePlayer;
+            if (!PLAYER_MAP.containsKey(level)) {
+                fakePlayer = new FakePlayer(serverLevel, FAKE_PROFILE);
+                PLAYER_MAP.put(serverLevel, fakePlayer);
+            } else {
+                fakePlayer = PLAYER_MAP.get(level);
             }
+            return fakePlayer;
+        } else {
+            return Minecraft.getInstance().player;
         }
     }
+
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ModEvents{
         @SubscribeEvent
--- src/main/java/io/github/ageuxo/ATMQuarryPatch/Mixins/MixinBlockBreakingMovementBehaviour.java
+++ src/main/java/io/github/ageuxo/ATMQuarryPatch/Mixins/MixinBlockBreakingMovementBehaviour.java
@@ -1,19 +1,16 @@
 package io.github.ageuxo.ATMQuarryPatch.Mixins;
 
-import com.mojang.authlib.GameProfile;
 import com.simibubi.create.content.kinetics.base.BlockBreakingMovementBehaviour;
 import com.simibubi.create.foundation.utility.BlockHelper;
 import com.thevortex.allthemodium.registry.ModRegistry;
+import io.github.ageuxo.ATMQuarryPatch.ATMQuarryPatch;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.common.util.FakePlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.UUID;
 import java.util.function.Consumer;
 
 @Mixin(BlockBreakingMovementBehaviour.class)
@@ -23,8 +20,7 @@ public abstract class MixinBlockBreakingMovementBehaviour {
             method = "destroyBlock",
             at = @At(value = "INVOKE", target = "Lcom/simibubi/create/foundation/utility/BlockHelper;destroyBlock(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;FLjava/util/function/Consumer;)V"))
     private void destroyBlock(Level world, BlockPos pos, float effectChance, Consumer<ItemStack> droppedItemCallback) {
-        FakePlayer atmQuarryPatchFakePlayer = new FakePlayer((ServerLevel) world, new GameProfile(UUID.randomUUID(), "ATMQuarryPatch FakePlayer"));
         ItemStack usedTool = new ItemStack(ModRegistry.ALLTHEMODIUM_PICKAXE.get());
-        BlockHelper.destroyBlockAs(world, pos, atmQuarryPatchFakePlayer, usedTool, effectChance, droppedItemCallback);
+        BlockHelper.destroyBlockAs(world, pos, ATMQuarryPatch.getFakePlayer(world), usedTool, effectChance, droppedItemCallback);
     }
 }
